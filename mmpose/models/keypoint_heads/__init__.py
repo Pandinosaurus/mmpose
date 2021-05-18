c1b888c5a8aad73166a61677a8b382bb9cc24159
luminxu <luminxu@link.cuhk.edu.hk>
2021-05-18 23:49:36 +0800

    add paf detector & head & loss

--- mmpose/models/keypoint_heads/__init__.py
+++ mmpose/models/keypoint_heads/__init__.py
@@ -4,13 +4,15 @@ from .fc_head import FcHead
 from .heatmap_1d_head import Heatmap1DHead
 from .heatmap_3d_head import HeatMap3DHead
 from .multilabel_classification_head import MultilabelClassificationHead
+from .paf_head import PAFHead
+from .paf_simple_head import PAFSimpleHead
 from .temporal_regression_head import TemporalRegressionHead
 from .top_down_multi_stage_head import TopDownMSMUHead, TopDownMultiStageHead
 from .top_down_simple_head import TopDownSimpleHead
 
 __all__ = [
     'TopDownSimpleHead', 'TopDownMultiStageHead', 'TopDownMSMUHead',
-    'BottomUpHigherResolutionHead', 'BottomUpSimpleHead', 'FcHead',
-    'TemporalRegressionHead', 'HeatMap3DHead', 'Heatmap1DHead',
-    'MultilabelClassificationHead'
+    'BottomUpHigherResolutionHead', 'BottomUpSimpleHead', 'PAFHead',
+    'PAFSimpleHead', 'FcHead', 'TemporalRegressionHead', 'HeatMap3DHead',
+    'Heatmap1DHead', 'MultilabelClassificationHead'
 ]
